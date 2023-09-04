47d33347471c776f107c3577deeca8295a5eac4c
carlasilveira <casilveirag@hotmail.com>
2023-09-04 20:28:30 -0300

    cambios

--- Formulario.js
+++ Formulario.js
@@ -1,11 +1,33 @@
-const URL= "https://jsonplaceholder.typicode.com/users"
+// Obtener el formulario y agregar un controlador de eventos para el envío
+const form = document.querySelector('form');
+form.addEventListener('submit', event => {
+  // Prevenir el comportamiento predeterminado del formulario
+  event.preventDefault();
 
-fetch('<URL>', {
+  // Crear un objeto FormData y pasarlo al formulario
+  const formData = new FormData(form);
+
+  // Convertir el objeto FormData en un objeto JSON
+  const data = {};
+  for (const [key, value] of formData.entries()) {
+    data[key] = value;
+  }
+
+  // Enviar los datos en formato JSON al endpoint especificado
+  const URL= "https://jsonplaceholder.typicode.com/users"
+  fetch(URL, {
     method: 'POST',
     headers: {
-        'Content-Type': 'application/json'
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
-})
-.then(response => response.json())
-.then(data => console.log(data));
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+});
+
+
+
+
+
