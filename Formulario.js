8cf22b64d26056f2a45b4c4c22ce7f5eb9827c56
carlasilveira <casilveirag@hotmail.com>
2023-09-04 20:22:01 -0300

    Update Formulario.js

---
+++ Formulario.js
@@ -0,0 +1,11 @@
+const URL= "https://jsonplaceholder.typicode.com/users"
+
+fetch('<URL>', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+})
+.then(response => response.json())
+.then(data => console.log(data));
